33b76b000a8d9d739e34a8c4c381623feaa610f1
Gian Luca <55563130+GLPG35@users.noreply.github.com>
2023-02-02 19:14:07 -0300

    v0.2.0
    Add turborepo

---
+++ apps/server/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+const PORT = process.env.PORT || 3000
+
+app.get('/', (_req, res) => {
+	res.send('Nazi')
+})
+
+app.listen(PORT)
