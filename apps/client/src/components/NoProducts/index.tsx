31aba8e4927657882e5dbff54ae5989fb09d3cea
Gian Luca <55563130+GLPG35@users.noreply.github.com>
2023-05-25 01:03:52 -0300

    v6.0.1
    Add sign when there are no orders in the orders page

--- apps/client/src/components/NoProducts/index.tsx
+++ apps/client/src/components/NoProducts/index.tsx
@@ -1,17 +1,18 @@
 import { CSSProperties } from 'react'
-import { TbFilesOff, TbShoppingCartOff } from 'react-icons/tb'
+import { TbFilesOff, TbShoppingCartOff, TbTicketOff } from 'react-icons/tb'
 import styles from './styles.module.scss'
 import { useNavigate } from 'react-router-dom'
 
 const NoProducts = ({ title, message, style, icon, buttonMsg, buttonLink, buttonIcon }:
-{ title: string, message: string, style?: CSSProperties, icon?: 'cart' | 'product', buttonMsg?: string, buttonLink?: string, buttonIcon?: JSX.Element }) => {
+{ title: string, message: string, style?: CSSProperties, icon?: 'cart' | 'product' | 'order', buttonMsg?: string, buttonLink?: string, buttonIcon?: JSX.Element }) => {
 	const navigate = useNavigate()
 
 	return (
 		<div className={styles.noProducts} style={style}>
 			<div className={styles.icon}>
 				{!icon || icon == 'product' ? <TbFilesOff />
-				: icon == 'cart' && <TbShoppingCartOff />}
+				: icon == 'cart' ? <TbShoppingCartOff />
+				: icon == 'order' && <TbTicketOff />}
 			</div>
 			<div className={styles.title}>
 				{title}
--- apps/client/src/pages/Orders/index.tsx
+++ apps/client/src/pages/Orders/index.tsx
@@ -3,12 +3,14 @@ import styles from './styles.module.scss'
 import { manageOrder } from '../../utils/server'
 import { globalContext } from '../../App'
 import { Order } from '../../types'
-import { TbChevronRight, TbCurrencyDollar, TbDisc } from 'react-icons/tb'
+import { TbChevronRight, TbCurrencyDollar, TbDisc, TbHome2, TbShoppingCart } from 'react-icons/tb'
 import { useNavigate } from 'react-router-dom'
 import { motion } from 'framer-motion'
+import NoProducts from '../../components/NoProducts'
+import Spinner from '../../components/Spinner'
 
 const Orders = () => {
-	const { user } = useContext(globalContext)
+	const { user, cartCount } = useContext(globalContext)
 	const [orders, setOrders] = useState<Order[]>()
 	const [hoverOrder, setHoverOrder] = useState<string>()
 	const navigate = useNavigate()
@@ -17,7 +19,6 @@ const Orders = () => {
 		if (orders === undefined && user) {
 			manageOrder(user.email)
 			.then(res => {
-				console.log(res)
 				setOrders(res)
 			})
 		}
@@ -32,51 +33,64 @@ const Orders = () => {
 			<div className={styles.title}>
 				Orders
 			</div>
-			<div className={styles.ordersList}>
-				{orders && orders.map(({ id, amount, createdAt, products }) => {
-					const total = products.reduce((prev, curr) => {
-						return prev + curr.quantity
-					}, 0)
+			{orders === undefined ?
+				<div className={styles.spinner}>
+					<Spinner color='secondary' background style={{ height: '100%' }} />
+				</div>
+			: orders.length > 0 ?
+				<div className={styles.ordersList}>
+					{orders.map(({ id, amount, createdAt, products }) => {
+						const total = products.reduce((prev, curr) => {
+							return prev + curr.quantity
+						}, 0)
 
-					const date = new Intl.DateTimeFormat('en-UK', {
-						dateStyle: 'medium',
-						timeStyle: 'short'
-					}).format(new Date(createdAt))
-					
-					return (
-						<div className={styles.order} key={id}
-						onClick={() => handleOrder(id)} onMouseEnter={() => setHoverOrder(id)}
-						onMouseLeave={() => setHoverOrder(undefined)}>
-							<div className={styles.background}>
-								<div className={styles.icon}>
-									<TbDisc />
-								</div>
-							</div>
-							<div className={styles.orderTitle}>
-								<div className={styles.orderId}>
-									Order #{id}
-								</div>
-								<div className={styles.date}>
-									{date}
-								</div>
-							</div>
-							<div className={styles.bottom}>
-								<div className={styles.info}>
-									<div className={styles.total}>
-										<TbDisc /> {total}
-									</div>
-									<div className={styles.amount}>
-										<TbCurrencyDollar /> {amount}
+						const date = new Intl.DateTimeFormat('en-UK', {
+							dateStyle: 'medium',
+							timeStyle: 'short'
+						}).format(new Date(createdAt))
+						
+						return (
+							<div className={styles.order} key={id}
+							onClick={() => handleOrder(id)} onMouseEnter={() => setHoverOrder(id)}
+							onMouseLeave={() => setHoverOrder(undefined)}>
+								<div className={styles.background}>
+									<div className={styles.icon}>
+										<TbDisc />
 									</div>
 								</div>
-								<motion.button animate={{ y: hoverOrder == id ? 0 : 100 }}>
-									<TbChevronRight />
-								</motion.button>
+								<div className={styles.orderTitle}>
+									<div className={styles.orderId}>
+										Order #{id}
+									</div>
+									<div className={styles.date}>
+										{date}
+									</div>
+								</div>
+								<div className={styles.bottom}>
+									<div className={styles.info}>
+										<div className={styles.total}>
+											<TbDisc /> {total}
+										</div>
+										<div className={styles.amount}>
+											<TbCurrencyDollar /> {amount}
+										</div>
+									</div>
+									<motion.button initial={{ y: 100 }} animate={{ y: hoverOrder == id ? 0 : 100 }}>
+										<TbChevronRight />
+									</motion.button>
+								</div>
 							</div>
-						</div>
-					)
-				})}
-			</div>
+						)
+					})}
+				</div>
+			:
+				<NoProducts title="You haven't purchased anything yet"
+				message={cartCount ? 'Please complete the pending purchase' : 'Go fill your cart with some albums'}
+				buttonIcon={cartCount ? <TbShoppingCart /> : <TbHome2 />}
+				buttonMsg={cartCount ? 'Go to cart' : 'Go home'}
+				buttonLink={cartCount ? '/cart' : '/'}
+				icon='order' />
+			}
 		</div>
 	)
 } 
